502ef5fdaeee2b6693c947635acc2fd6ae5e446e
Rob Hope <rob@onepagelove.com>
2017-07-20 17:10:20 +0200

    v0.0.11 - TGM Plugin Activation
    - Recommending SiteOrigin Page Builder and Widgets Bundle

--- fullsingle.php
+++ fullsingle.php
@@ -2,7 +2,7 @@
 
 /**
  * @package FullSingle
- * @version 0.0.10
+ * @version 0.0.11
  * @license GPL 2.0
  *
 Plugin Name: FullSingle
@@ -10,7 +10,7 @@ Plugin URI: https://onepagelove.com/fullsingle
 Description: Beautiful Landing Page layouts for SiteOrigin's Free Page Builder.
 Author: One Page Love
 Author URI: https://onepagelove.com
-Version: 0.0.10
+Version: 0.0.11
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: fullsingle
@@ -29,3 +29,4 @@ require_once( $plugin_dir . '/functions/function-layouts.php'    				);  # Site 
 require_once( $plugin_dir . '/functions/function-enqueue-page-templates.php'    );  # Enqueue Page Templates
 require_once( $plugin_dir . '/functions/function-enqueue-stylesheets.php'    	);  # Enqueue Stylesheets
 require_once( $plugin_dir . '/functions/function-customizer.php'    			);  # Add Customizer Panel
+require_once( $plugin_dir . '/functions/function-tgm.php'                  		);  # Recommended Plugins
