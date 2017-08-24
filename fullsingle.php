0d5d3cc9ca0843ed0563e4c50382af20030f3a1b
Rob Hope <rob@onepagelove.com>
2017-08-24 17:10:52 +0200

    v0.5.0.1 - Improvements to Split layout
    - better vertical alignment to content column of Split
    - better nesting of Split SCSS styles, reduced to 5kb from 12kb
    - added more variables to Split SCSS styles

--- fullsingle.php
+++ fullsingle.php
@@ -3,7 +3,7 @@
 Plugin Name: FullSingle
 Plugin URI: https://onepagelove.com/fullsingle
 Description: Beautiful Free Landing Page layouts that work with any WordPress theme.
-Version: 0.5
+Version: 0.5.0.1
 Author: One Page Love
 Author URI: https://onepagelove.com
 License: GPLv2 or later
